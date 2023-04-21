10b59cefcf5b7a87d7cc2564671142de8e1da6ea
Victor Neznaykin <va.neznaykin@gmail.com>
2023-04-21 20:32:05 +0400

    process only messages

--- modules/authz/message.go
+++ modules/authz/message.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/query"
 	authztypes "github.com/cosmos/cosmos-sdk/x/authz"
 
 	"github.com/bro-n-bro/spacebox-crawler/types"
@@ -40,9 +39,6 @@ func (m *Module) HandleMessage(ctx context.Context, index int, cosmosMsg sdk.Msg
 			return err
 		}
 
-		if err := m.findAndPublishAuthzGrants(ctx, msg.Granter, msg.Grantee, tx.Height); err != nil {
-			return err
-		}
 	case *authztypes.MsgRevoke:
 		if err := m.broker.PublishRevokeMessage(ctx, model.RevokeMessage{
 			Height:   tx.Height,
@@ -55,9 +51,6 @@ func (m *Module) HandleMessage(ctx context.Context, index int, cosmosMsg sdk.Msg
 			return err
 		}
 
-		if err := m.findAndPublishAuthzGrants(ctx, msg.Granter, msg.Grantee, tx.Height); err != nil {
-			return err
-		}
 	case *authztypes.MsgExec:
 		messages := make([][]byte, 0, len(msg.Msgs))
 		for _, message := range msg.Msgs {
@@ -75,55 +68,6 @@ func (m *Module) HandleMessage(ctx context.Context, index int, cosmosMsg sdk.Msg
 			Msgs:     messages,
 		}); err != nil {
 			return err
-		}
-	}
-
-	return nil
-}
-
-// findAndPublishAuthzGrants finds all authz grants for the given granter and grantee and publishes data to the broker.
-func (m *Module) findAndPublishAuthzGrants(ctx context.Context, granter, grantee string, height int64) error {
-	var nextKey []byte
-
-	for {
-		respPb, err := m.client.AuthzQueryClient.Grants(
-			ctx,
-			&authztypes.QueryGrantsRequest{
-				Granter: granter,
-				Grantee: grantee,
-				Pagination: &query.PageRequest{
-					Key:   nextKey,
-					Limit: 150,
-				},
-			},
-		)
-		if err != nil {
-			return err
-		}
-
-		nextKey = respPb.Pagination.NextKey
-		for _, grant := range respPb.Grants {
-			ag := model.AuthzGrant{
-				Height:         height,
-				GranterAddress: granter,
-				GranteeAddress: grantee,
-			}
-			if grant.Expiration != nil {
-				ag.Expiration = *grant.Expiration
-			}
-
-			if grant.Authorization != nil {
-				ag.MsgType = grant.Authorization.TypeUrl
-			}
-
-			if err = m.broker.PublishAuthzGrant(ctx, ag); err != nil {
-				m.log.Err(err).Int64("height", height).Msg("error while publishing authz grant")
-				return err
-			}
-		}
-
-		if len(respPb.Pagination.NextKey) == 0 {
-			break
 		}
 	}
 
