9dbcd8ce01add1f9454fbd28fb4f291afe1efb7f
Reuk Aleksey Sergeevich (reuk.aleksey) <4c1d4u@gmail.com>
2023-10-24 21:30:33 +0400

    codestyle

--- modules/grid/broker.go
+++ modules/grid/broker.go
@@ -9,10 +9,9 @@ import (
 type broker interface {
 	PublishGridParams(context.Context, model.GridParams) error
 
+	PublishRoute(context.Context, model.Route) error
 	PublishCreateRouteMessage(context.Context, model.CreateRouteMessage) error
 	PublishEditRouteMessage(context.Context, model.EditRouteMessage) error
 	PublishEditRouteNameMessage(context.Context, model.EditRouteNameMessage) error
 	PublishDeleteRouteMessage(context.Context, model.DeleteRouteMessage) error
-
-	PublishRoute(context.Context, model.Route) error
 }
