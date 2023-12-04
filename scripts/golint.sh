f1105d0ce7b064bf3219d71f683b312da6b68fc5
Victor Neznaykin <va.neznaykin@gmail.com>
2023-12-04 19:58:25 +0400

    fix lint flow

--- scripts/golint.sh
+++ scripts/golint.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b bin v1.55.2
-bin/golangci-lint --version
 #https://golangci-lint.run/usage/quick-start/
 bin/golangci-lint run -v --config golangci.yml --timeout 5m
