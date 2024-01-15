55edd0b5dd24490cd4466f355942342f864cdabe
Victor Neznaykin <va.neznaykin@gmail.com>
2024-01-15 20:29:37 +0400

    add MAX_MESSAGE_MAX_BYTES env to config

--- delivery/broker/config.go
+++ delivery/broker/config.go
@@ -4,6 +4,7 @@ type (
 	Config struct {
 		ServerURL       string `env:"BROKER_SERVER"`
 		PartitionsCount int    `env:"PARTITIONS_COUNT" envDefault:"1"`
+		MaxMessageBytes int    `env:"MAX_MESSAGE_MAX_BYTES" envDefault:"5242880"` // 5MB
 		Enabled         bool   `env:"BROKER_ENABLED"`
 	}
 )
